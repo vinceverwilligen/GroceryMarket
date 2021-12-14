3505f93fd30b212588a609fce0be0bdc59617f67
Vince Verwilligen <david.vinke@hotmail.com>
2021-12-14 19:39:34 +1300

    Move classes to a ClassLibrary project

---
+++ GroceryMarketCon/Models/PointOfSaleTerminal.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryMarket.Models
+{
+    /// <summary>
+    /// Class <c>PointOfSaleTerminal</c> is the terminal which scans products.
+    /// </summary>
+    public class PointOfSaleTerminal
+    {
+        const string PRODUCT_WITH_CODE_NOT_FOUND = "Product with code {0} not found.";
+
+        private Dictionary<Product, int> shoppingCart;
+        /// <value>
+        /// Property <c>Products</c> represents a dictionary of scanned products and the count.
+        /// </value>
+        public Dictionary<Product, int> ShoppingCart
+        {
+            get { return shoppingCart; }
+            set { this.shoppingCart = value; }
+        }
+
+        /// <summary>
+        /// This constructor creates a new terminal with empty <c>ShoppingCart</c>.
+        /// </summary>
+        public PointOfSaleTerminal()
+        {
+            this.shoppingCart = new Dictionary<Product, int>();
+        }
+
+        /// <summary>
+        /// This method will scan a <c>Product</c> from the <c>GroceryStore</c> and add it to the <c>ShoppingCart</c>.
+        /// </summary>
+        /// <param name="code"><c>code</c> is the code of the product.</param>
+        /// <param name="groceryStore"><c>groceryStore</c> contains the available products to scan.</param>
+        /// <returns>The product that is scanned.</returns>
+        /// <exception cref="Exception">Throws an exception if <c>Product</c> with <c>code</c> is not found.</exception>
+        public Product ScanProduct(string code, GroceryStore groceryStore)
+        {
+            if (!groceryStore.Products.ContainsKey(code))
+            {
+                throw new Exception(String.Format(PRODUCT_WITH_CODE_NOT_FOUND, code));
+            }
+
+            Product product = groceryStore.Products[code];
+            if (this.shoppingCart.ContainsKey(product))
+            {
+                this.shoppingCart[product] += 1;
+            }
+            else
+            {
+                this.shoppingCart.Add(product, 1);
+            }
+
+            return product;
+        }
+
+        /// <summary>
+        /// This method scans multiple products after eachother.
+        /// </summary>
+        /// <param name="codes"><c>codes</c> is the list of codes that will be scanned.</param>
+        /// <param name="groceryStore"><c>groceryStore</c> contains the available products to scan.</param>
+        public void ScanProducts(string codes, GroceryStore groceryStore)
+        {
+            foreach(char code in codes)
+            {
+                ScanProduct(code.ToString(), groceryStore);
+            }
+        }
+
+        /// <summary>
+        /// This method will empty the <c>ShoppingCart</c>.
+        /// </summary>
+        public void CleanShoppingCart()
+        {
+            this.shoppingCart.Clear();
+        }
+
+        /// <summary>
+        /// This method will calculate the total of all products in the <c>ShoppingCart</c>.
+        /// </summary>
+        /// <returns>The total price of the <c>ShoppingCart</c></returns>
+        public double CalculateResult()
+        {
+            double total = 0;
+            foreach (KeyValuePair<Product, int> kvp in this.ShoppingCart)
+            {
+                Product product = kvp.Key;
+                int units = kvp.Value;
+
+                total += product.CalculateProductPrice(units);
+            }
+
+            return total;
+        }
+    }
+}
---
+++ GroceryMarketCon/Models/Product.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryMarket.Models
+{
+    /// <summary>
+    /// Class <c>Product</c> represents a <c>Product</c> available in the <c>GroceryStore</c>.
+    /// </summary>
+    public class Product
+    {
+
+        const string PRODUCT_WITH_CODE_VOLUME_PRICE_NOT_FOUND = "Product with code {0} has no value price for {0} units.";
+
+        private string code;
+        /// <value>
+        /// Property <c>Code</c> is the code used for scanning the product.
+        /// </value>
+        public string Code
+        {
+            get { return code; }
+            set { this.code = value; }
+        }
+
+        private string name;
+        /// <value>
+        /// Propery <c>name</c> is the name of the <c>Product</c>.
+        /// </value>
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+
+        private Dictionary<int, Price> prices;
+        /// <value>
+        /// The property <c>Prices</c> contains the unit price and all other volume prices.
+        /// </value>
+        public Dictionary<int, Price> Prices
+        {
+            get { return prices; }
+            set { this.prices = value; }
+        }
+
+        /// <summary>
+        /// This constructor will create a <c>Product</c> with <c>code</c>, <c>name</c> and <c>unitPrice</c>.
+        /// </summary>
+        /// <param name="code"><c>code</c> is the code of the <c>Product</c>.</param>
+        /// <param name="name"><c>name</c> is the name of the <c>Product</c>.</param>
+        /// <param name="unitPrice"><c>unitPrice</c> is the unit price of the <c>Product</c>.</param>
+        public Product(string code, string name, double unitPrice)
+        {
+            this.code = code;
+            this.name = name;
+            this.prices = new Dictionary<int, Price>();
+            this.prices.Add(1, new Price(1, unitPrice));
+        }
+
+        /// <summary>
+        /// This methos will set or update the <c>price</c> for the given volume of <c>units</c>.
+        /// </summary>
+        /// <param name="price">Is the new <c>price</c> of the <c>Product</c>.</param>
+        /// <param name="units">Is the amount of <c>units</c> for the given price.</param>
+        public void SetVolumePrice(double price, int units)
+        {
+            if (this.prices.ContainsKey(units))
+            {
+                this.prices[units].Value = price;
+            }
+            else
+            {
+                this.prices.Add(units, new Price(units, price));
+            }
+        }
+
+        /// <summary>
+        /// This method with find the price for the amount of <c>units</c>.
+        /// </summary>
+        /// <param name="units">The <c>units</c> of the <c>Product</c>.</param>
+        /// <returns>Returns the price for the given amount of units.</returns>
+        /// <exception cref="Exception">Not price found fot the given <c>units</c>.</exception>
+        public double GetVolumePrice(int units)
+        {
+            if (!this.prices.ContainsKey(units))
+            {
+                throw new Exception(string.Format(PRODUCT_WITH_CODE_VOLUME_PRICE_NOT_FOUND, code, units));
+            }
+
+            return this.prices[units].Value;
+        }
+
+        /// <summary>
+        /// Get the highest amount of volume price available.
+        /// </summary>
+        /// <returns>The units of the volume.</returns>
+        private int GetHighestVolume()
+        {
+            if (this.Prices.Count == 1)
+            {
+                return 1;
+            }
+
+            List<int> volumes = this.prices.Keys.ToList();
+            volumes.Sort();
+            return volumes[volumes.Count - 1];
+        }
+
+        /// <summary>
+        /// Get the highest amount of volume price available lower than given amount of <c>units</c>.
+        /// </summary>
+        /// <param name="units">The <c>units</c> of the <c>Product</c>.</param>
+        /// <returns>Returns the highest amount of units lower than given <c>units</c>.</returns>
+        private int GetVolumeLowerThan(int units)
+        {
+            if (this.Prices.Count == 1 || units == 1)
+            {
+                return 1;
+            }
+
+            List<int> unitsList = this.prices.Keys.ToList();
+            unitsList.Sort();
+
+            int index = unitsList.IndexOf(units);
+            return unitsList[index - 1];
+        }
+        /// <summary>
+        /// This method calculate the total amount for given <c>units</c> of a <c>Product</c>.
+        /// </summary>
+        /// <param name="units">The <c>units</c> of the <c>Product</c>.</param>
+        /// <returns>Return the total price of the <c>units</c> of the <c>Product</c>.</returns>
+        public double CalculateProductPrice(int units)
+        {
+            double total = 0;
+
+            int count = units;
+            int priceVolume = GetHighestVolume();
+            while (count > 0)
+            {
+                if (priceVolume == count)
+                {
+                    total += this.prices[priceVolume].Value;
+                    count -= priceVolume;
+                }
+                else if (priceVolume <= count)
+                {
+                    total += this.prices[priceVolume].Value;
+                    count -= priceVolume;
+                }
+                else if (priceVolume > 1)
+                {
+                    priceVolume = GetVolumeLowerThan(priceVolume);
+                }
+            }
+
+            return total;
+        }
+    }
+}
